98a00287d613dc31810c8288cc5cf981d80b8c31
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-11-19 13:53:37 -0500

    done

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,12 @@
 function fibonacci(n) {
   // type your code here
+  //make a function that makes the fibonacci sequence
+  const fib = [0,1]
+  //takes in an array, checks if it's n-1 long, if so, returns that number
+  function buildFib(arr,count) {
+    return count===n ? arr[1] : buildFib([arr[1],arr[0]+arr[1]],++count)
+  }
+  return n===0 ? 0 : buildFib(fib,1);
 }
 
 if (require.main === module) {
